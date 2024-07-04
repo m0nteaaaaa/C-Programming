ef091bdba80aa97a8e9ec91cb2f0903cd5844179
Mehul Adhikari <155364569+m0nteaaaaa@users.noreply.github.com>
2024-07-04 11:11:39 +0530

    Add files via upload

---
+++ C Programming/BASICS/AreaOfACircle.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+int main(){
+    float r = 5;
+    float pi = 3.1415;
+    float area = pi*r*r;
+    printf("The radius of the circle : %f", area);
+    return 0;
+}
+/*
+To find the area of circle 
+area = pi*r*r
+*/
