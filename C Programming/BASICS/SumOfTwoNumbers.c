e3acdbb8c7b98bf13381d60baa9c8b77e432d051
Mehul Adhikari <155364569+m0nteaaaaa@users.noreply.github.com>
2024-07-08 23:13:04 +0530

    Add files via upload

---
+++ C Programming/BASICS/SumOfTwoNumbers.c
@@ -0,0 +1,15 @@
+/*
+To find sum of two numbers
+*/
+#include<stdio.h>
+int main(){
+    int x;
+    printf("enter the First Number : ");
+    scanf("%d",&x);
+    int y;
+    printf("Enter the Second Number : ");
+    scanf("%d",&y);
+    int sum = x+y;
+    printf("The Sum is : %d",sum);
+    return 0;
+}
