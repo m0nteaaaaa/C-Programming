8e156a7aa958f18aa37da34741848b999230bd17
Mehul Adhikari <155364569+m0nteaaaaa@users.noreply.github.com>
2024-07-09 17:23:09 +0530

    Add files via upload

---
+++ C Programming/BASICS/VolumeOfCircle.c
@@ -0,0 +1,9 @@
+//To print out volume of a circle
+#include<stdio.h>
+ int main(){
+    float r = 2;
+    float p = 3.14;
+    float v = ( 4 * p * r * r * r )/3;
+    printf("The volume of Circle : %f m^3", v) ;
+    return 0;
+ }
