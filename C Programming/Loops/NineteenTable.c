02f4921b78b9e29e724b1d95497fa63a6b76b95f
Mehul Adhikari <155364569+m0nteaaaaa@users.noreply.github.com>
2024-07-13 17:57:30 +0530

    Add files via upload

---
+++ C Programming/Loops/NineteenTable.c
@@ -0,0 +1,11 @@
+//Print the Ninteen Table
+#include<stdio.h>
+int main(){
+    int t=19;
+    int n;
+    for (int i =1; i<=10;i++){
+        n = t * i;
+        printf("\n%d",n);
+    }
+    return 0;
+}
